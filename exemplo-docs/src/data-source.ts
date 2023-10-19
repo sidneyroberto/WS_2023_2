d683b5dfa1f5c942071ee9a896b96aacd3cb4678
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-10-18 20:19:36 -0400

    finalizando docs

--- exemplo-docs/src/data-source.ts
+++ exemplo-docs/src/data-source.ts
@@ -11,3 +11,11 @@ export const AppDataSource = new DataSource({
   migrations: [],
   subscribers: [],
 })
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!')
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err)
+  })
